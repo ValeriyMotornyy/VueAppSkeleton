a1978b4d988739dda43c546a047295aeeb293e0f
tomaszthreadable <tomasz@mrozinski.net>
2020-06-27 16:49:27 +0100

    fix missing loaders from less

--- tasks/development/webpack.config.js
+++ tasks/development/webpack.config.js
@@ -73,6 +73,7 @@ module.exports = {
                 test: /\.less$/,
                 use: [
                     'vue-style-loader',
+                    'css-loader',
                     'less-loader'
                 ]
             },
